88abb7b8b69d5b42df4ab6a2129ac2ddc7918f6f
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-24 11:04:47 -0500

    Se reutilizo archivos de proyectos anteriores

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var userFullName = '';
+var userKey = '';
+
+window.addEventListener('load', showWelcome);
+function showWelcome(){
+  let html = '<h2>Bienvenido ' + userFullName + '</h2>\n';
+  html += `
+          <p>Este sistema fue desarrollado por alumnos del primer año de la Escuela Profesional de Ingeniería de Sistemas, de la Universidad Nacional de San Agustín de Arequipa</p>
+          <p>El sistema fué desarrollado usando estas tecnologías:</p>
+          <ul>
+            <li>HTML y CSS</li>
+            <li>Node.js para el backend</li>
+            <li>Javascript para el frontend</li>
+            <li>Las páginas se escriben en lenguaje Markdown</li>
+            <li>Se uso un libreria de node para el procesamiento del lenguaje Markdown</li>
+          </ul>`;
+  document.getElementById('main').innerHTML = html;
+}
+
+function showMenuUserLogged(){
+  let html = "<p onclick='showWelcome()'>Inicio</p>\n"+
+    "<p onclick='doList()'>Lista de Páginas</p>\n"+
+    "<p onclick='showNew()' class='rightcolumn'>Página Nueva</p>\n"
+  document.getElementById('menu').innerHTML = html;
+}
+
